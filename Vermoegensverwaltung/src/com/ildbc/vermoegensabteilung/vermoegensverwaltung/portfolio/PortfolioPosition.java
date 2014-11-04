5a5146547f437ec89bc9ca499b6e0c777dce55ab
Felix Navas <felixmorillasnavas@gmail.com>
2014-11-04 18:11:46 +0100

    portfolioposition

--- Vermoegensverwaltung/src/com/ildbc/vermoegensabteilung/vermoegensverwaltung/portfolio/PortfolioPosition.java
+++ Vermoegensverwaltung/src/com/ildbc/vermoegensabteilung/vermoegensverwaltung/portfolio/PortfolioPosition.java
@@ -1,47 +1,23 @@
-package model.com.ildbc.vermoegensabteilung.vermoegensverwaltung.portfolio;
+package com.ildbc.vermoegensabteilung.vermoegensverwaltung.portfolio;
 
 
 
-/**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
- */
+
 public  interface PortfolioPosition 
 {
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
+	
 	
 	public getAnzahl() ;
 	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
+	
 	
 	public getName() ;
 	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
+	
 	
 	public getTyp() ;
 	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
+	
 	
 	public getZeitwert(int zeit) ;
 	
