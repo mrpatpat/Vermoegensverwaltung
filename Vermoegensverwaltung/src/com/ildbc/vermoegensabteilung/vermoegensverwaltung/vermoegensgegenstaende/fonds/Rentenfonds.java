98ff6349cbb09e79b862af873e87863f28e73a7a
sky2107 <felixmorillasnavas@gmail.com>
2014-11-05 12:31:46 +0100

    Update Rentenfonds.java
    Kommentare

--- Vermoegensverwaltung/src/com/ildbc/vermoegensabteilung/vermoegensverwaltung/vermoegensgegenstaende/fonds/Rentenfonds.java
+++ Vermoegensverwaltung/src/com/ildbc/vermoegensabteilung/vermoegensverwaltung/vermoegensgegenstaende/fonds/Rentenfonds.java
@@ -4,6 +4,16 @@ import java.util.List;
 
 import com.ildbc.vermoegensabteilung.vermoegensverwaltung.vermoegensgegenstaende.rentenpapiere.Rentenpapier;
 
+/**
+ * Ansammlung der Rentenpapiere
+ * Klasse Rentenfonds
+ * erbt von Fonds
+ * 
+ * @author Adrian, Marcel und Felix
+ *
+ * date 5.11.2014
+ */
+
 public class Rentenfonds extends Fonds<Rentenpapier> {
 
 	public Rentenfonds(int kaufdatum, long kaufpreis, String name, int anzahl, List<Rentenpapier> rentenpapiere) {
