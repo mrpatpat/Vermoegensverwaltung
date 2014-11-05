a05f9ab62a9cb8474358a4615ba74cd2d53dc07d
Felix Navas <felixmorillasnavas@gmail.com>
2014-11-05 16:48:49 +0100

    noch mehr kommentare

--- Vermoegensverwaltung/src/com/ildbc/vermoegensabteilung/vermoegensverwaltung/vermoegensgegenstaende/rentenpapiere/Rentenpapier.java
+++ Vermoegensverwaltung/src/com/ildbc/vermoegensabteilung/vermoegensverwaltung/vermoegensgegenstaende/rentenpapiere/Rentenpapier.java
@@ -3,23 +3,56 @@ package com.ildbc.vermoegensabteilung.vermoegensverwaltung.vermoegensgegenstaend
 import com.ildbc.vermoegensabteilung.vermoegensverwaltung.vermoegensgegenstaende.Handelbar;
 import com.ildbc.vermoegensabteilung.vermoegensverwaltung.vermoegensgegenstaende.festgelder.Festgeld;
 
+/**
+ * Representation des Rentenpapiers
+ * 
+ * @author Adrian, Marcel und Felix
+ *
+ *date 5.11.2014
+ *
+ */
+
 public class Rentenpapier extends Festgeld implements Handelbar {
 
 	private long kurs;
+	
+	/**
+	 * Konstrukteur des Rentenpapiers
+	 * 
+	 * @param kaufdatum
+	 * @param kaufpreis
+	 * @param name
+	 * @param anzahl
+	 * @param laufzeit
+	 * @param zins
+	 * @param kurs
+	 */
 
 	public Rentenpapier(int kaufdatum, long kaufpreis, String name, int anzahl, int laufzeit, double zins, long kurs) {
 		super(kaufdatum, kaufpreis, name, anzahl, laufzeit, zins);
 		this.kurs = kurs;
 	}
+	
+	/**
+	 * Endwert ist der Kurs
+	 */
 
 	@Override
 	public long getEndwert() {
 		return this.getKurs();
 	}
 
+	/**
+	 * Kurs wird angegeben
+	 */
+	
 	public long getKurs() {
 		return kurs;
 	}
+	
+	/**
+	 * 
+	 */
 
 	public void verkaufen() {
 	}
@@ -27,9 +60,17 @@ public class Rentenpapier extends Festgeld implements Handelbar {
 	public void kaufen() {
 	}
 
+	/**
+	 * Typ wird angegeben
+	 */
+	
 	public String getTyp() {
 		return "Rentenpapier";
 	}
+	
+	/**
+	 * Kurs wird gesetzt
+	 */
 
 	@Override
 	public void setKurs(long kurs) {
