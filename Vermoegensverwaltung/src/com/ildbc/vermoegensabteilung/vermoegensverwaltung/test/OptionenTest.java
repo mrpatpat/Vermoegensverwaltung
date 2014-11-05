7aba41ca2b985644d604752b6632c7fb6a6a4c94
Felix Navas <felixmorillasnavas@gmail.com>
2014-11-05 22:00:31 +0100

    test

--- Vermoegensverwaltung/src/com/ildbc/vermoegensabteilung/vermoegensverwaltung/test/OptionenTest.java
+++ Vermoegensverwaltung/src/com/ildbc/vermoegensabteilung/vermoegensverwaltung/test/OptionenTest.java
@@ -1,14 +1,15 @@
 package com.ildbc.vermoegensabteilung.vermoegensverwaltung.test;
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import com.ildbc.vermoegensabteilung.vermoegensverwaltung.vermoegensgegenstaende.aktien.Aktie;
+import com.ildbc.vermoegensabteilung.vermoegensverwaltung.vermoegensgegenstaende.optionen.Calloption;
 
 
 public class OptionenTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		//Calloption b = new Calloption(0, 1000, "BASF", 2,2,2,2,wieso hier);
 	}
 
 }
