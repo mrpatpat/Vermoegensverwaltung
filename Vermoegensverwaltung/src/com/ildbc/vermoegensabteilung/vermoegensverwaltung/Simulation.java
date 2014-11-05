0fc6dbc2a0f486ef11d40b21ef8ba6e3885fbbdb
mrpatpat <adrian.endrich@googlemail.com>
2014-11-05 20:51:06 +0100

    asdasd

--- Vermoegensverwaltung/src/com/ildbc/vermoegensabteilung/vermoegensverwaltung/Simulation.java
+++ Vermoegensverwaltung/src/com/ildbc/vermoegensabteilung/vermoegensverwaltung/Simulation.java
@@ -3,72 +3,20 @@ package com.ildbc.vermoegensabteilung.vermoegensverwaltung;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.ildbc.vermoegensabteilung.vermoegensverwaltung.portfolio.Portfolio;
+import com.ildbc.vermoegensabteilung.vermoegensverwaltung.vermoegensgegenstaende.Vermoegensgegenstand;
 import com.ildbc.vermoegensabteilung.vermoegensverwaltung.vermoegensgegenstaende.aktien.Aktie;
-import com.ildbc.vermoegensabteilung.vermoegensverwaltung.vermoegensgegenstaende.festgelder.Festgeld;
-import com.ildbc.vermoegensabteilung.vermoegensverwaltung.vermoegensgegenstaende.fonds.Aktienfonds;
-import com.ildbc.vermoegensabteilung.vermoegensverwaltung.vermoegensgegenstaende.immobilien.Immobilie;
-import com.ildbc.vermoegensabteilung.vermoegensverwaltung.vermoegensgegenstaende.immobilien.ImmobilieExistiertException;
-import com.ildbc.vermoegensabteilung.vermoegensverwaltung.vermoegensgegenstaende.rentenpapiere.Rentenpapier;
 
 public class Simulation {
-
-	private static final long[][] aktienDefinitionen = { { 11110, 12030 },
-			{ 6127, 8439 }, { 8824, 8267 }, { 9678, 9513 }, { 4993, 4512 },
-			{ 5867, 5756 }, { 11550, 12050 } };
-
-	private static final String[] aktienNamen = { "Muenchener Rueck", "BASF",
-			"MAN", "SIEMENS", "DAIMLER", "BMW", "VOLKSWAGEN" };
 
 	public static void main(String[] args) {
 		System.out.println("test");
 	}
 
-	private static final Immobilie createSchlossAllee(int kaufdatum)
-			throws ImmobilieExistiertException {
-		return new Immobilie(kaufdatum, 1000000, "Schlossallee 4, Heidelberg",
-				20);
-	}
-
-	private static Festgeld createSparkassenAnlage(int kaufdatum,
-			long kaufpreis, int anzahl, long kurs) {
-		return new Festgeld(kaufdatum, kaufpreis, "Sparkasse Mannheim", anzahl,
-				3, 2.0d);
-	}
-
-	private static Rentenpapier createBundesschatzbrief(int kaufdatum,
-			long kaufpreis, int anzahl, long kurs) {
-		return new Rentenpapier(kaufdatum, kaufpreis, "Bundesschatzbrief",
-				anzahl, 10, 1.5d, kurs);
-	}
-
-	private static Aktienfonds createAutofonds() {
-		List<Aktie> liste = new ArrayList<Aktie>();
-
-		liste.add(createAktie(4, 0, 500, 4, true));
-		liste.add(createAktie(4, 0, 500, 4, false));
-
-		liste.add(createAktie(3, 0, 500, 5, true));
-		liste.add(createAktie(3, 0, 500, 5, false));
-
-		liste.add(createAktie(5, 0, 500, 6, true));
-		liste.add(createAktie(5, 0, 500, 6, false));
-
-		return new Aktienfonds(0, 500, "Autofonds", 1, liste);
-	}
-
-	private static Aktienfonds createIndexFonds() {
-		List<Aktie> liste = new ArrayList<Aktie>();
-		for (int i = 0; i < aktienNamen.length; i++) {
-			liste.add(createAktie(1, 0, 500, i, true));
-			liste.add(createAktie(1, 0, 500, i, false));
-		}
-		return new Aktienfonds(0, 500, "Indexfonds", 1, liste);
-	}
-
-	private static Aktie createAktie(int anzahl, int kaufdatum, int kaufpreis,
-			int aktienName, boolean nutzeKurs1) {
-		return new Aktie(kaufdatum, kaufpreis, aktienNamen[aktienName], anzahl,
-				aktienDefinitionen[aktienName][nutzeKurs1 ? 0 : 1]);
+	private static Portfolio createPortfolioA(boolean istKurs1) {
+		List<Vermoegensgegenstand> vg = new ArrayList<Vermoegensgegenstand>();
+		vg.add(new Aktie(0, 6102, "BASF", 1000, istKurs1 ? 6127 : 8439));
+		return new Portfolio(vg);
 	}
 
 }
