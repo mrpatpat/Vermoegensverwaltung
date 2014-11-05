ea39163ae59eb4a0409bff8b56007abdf8809b0b
Felix Navas <felixmorillasnavas@gmail.com>
2014-11-05 21:46:58 +0100

    test

--- Vermoegensverwaltung/src/com/ildbc/vermoegensabteilung/vermoegensverwaltung/test/RentenpapierTest.java
+++ Vermoegensverwaltung/src/com/ildbc/vermoegensabteilung/vermoegensverwaltung/test/RentenpapierTest.java
@@ -1,14 +1,26 @@
 package com.ildbc.vermoegensabteilung.vermoegensverwaltung.test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+
+import com.ildbc.vermoegensabteilung.vermoegensverwaltung.vermoegensgegenstaende.rentenpapiere.Rentenpapier;
 
 
 public class RentenpapierTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Rentenpapier b = new Rentenpapier(0, 1000, "Rentenpapiere", 1, 20, 2, 3);
+		Rentenpapier c = new Rentenpapier(0, 2000, "Rentenpapiere", 1, 20, 2, 3);
+		
+		assertEquals(1000, b.getKaufpreis());
+		assertEquals(2000, c.getKaufpreis());
+
+		assertEquals("Rentenpapiere", b.getName());
+		assertEquals("Rentenpapiere", b.getName());
+
+		assertEquals(1, b.getAnzahl());
+		assertEquals(1, c.getAnzahl());
 	}
 
 }
