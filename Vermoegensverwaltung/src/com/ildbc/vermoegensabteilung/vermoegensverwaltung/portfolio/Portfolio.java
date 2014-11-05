3aa87547b6283a6890b9e07a5567df6d9d2c5802
mrpatpat <adrian.endrich@googlemail.com>
2014-11-05 20:13:04 +0100

    Portfoliomethoden + kommentare

--- Vermoegensverwaltung/src/com/ildbc/vermoegensabteilung/vermoegensverwaltung/portfolio/Portfolio.java
+++ Vermoegensverwaltung/src/com/ildbc/vermoegensabteilung/vermoegensverwaltung/portfolio/Portfolio.java
@@ -5,38 +5,83 @@ import java.util.List;
 import com.ildbc.vermoegensabteilung.vermoegensverwaltung.vermoegensgegenstaende.Vermoegensgegenstand;
 
 /**
- * Portfolio ist ene Ansammlung der Vermoegenswerte
- * Inhalt Aktien Festgelder Fonds Immobilien Rentenpapiere
- * + den Zusatz der Optionen
+ * Portfolio ist ene Ansammlung der Vermoegenswerte Inhalt Aktien Festgelder
+ * Fonds Immobilien Rentenpapiere + den Zusatz der Optionen
  * 
  * sie werden mit hilfe einer Liste generiet
  * 
  * @author Adrian, Marcel und Felix
- *date 5.11.2014
+ * @date 5.11.2014
  */
 
 public class Portfolio {
 
+	/**
+	 * Vermögenswerte
+	 */
 	private final List<Vermoegensgegenstand> vermoegenswerte;
 
+	/**
+	 * Konstruktor.
+	 * 
+	 * @param vermoegenswerte
+	 *            Liste aller Vermögenswerte des Portfolios
+	 */
 	public Portfolio(List<Vermoegensgegenstand> vermoegenswerte) {
 		this.vermoegenswerte = vermoegenswerte;
 	}
 
+	/**
+	 * Gibt die Liste der Portfoliopositionen zurück.
+	 * 
+	 * @return Portfoliopositionen
+	 */
 	public List<Vermoegensgegenstand> getVermoegenswerte() {
 		return this.vermoegenswerte;
 	}
 
+	/**
+	 * Gesamtinvestition des Portfolios.
+	 * 
+	 * @return Gesamtinvestition in Cent
+	 */
 	public long getGesamtinvestition() {
-		return 0L;
+
+		long investition = 0;
+
+		for (Vermoegensgegenstand v : vermoegenswerte) {
+			investition += v.getAnzahl() * v.getKaufpreis();
+		}
+
+		return investition;
 	}
 
+	/**
+	 * Zeitwert des Portfolios.
+	 * 
+	 * @param datum
+	 *            Zeitpunkt in Jahren
+	 * @return Zeitwert in Cent
+	 */
 	public long getZeitwert(int datum) {
-		return 0L;
+		long zeitwert = 0;
+
+		for (Vermoegensgegenstand v : vermoegenswerte) {
+			zeitwert += v.getAnzahl() * v.getZeitwert(datum);
+		}
+
+		return zeitwert;
 	}
 
-	public long getBilanz() {
-		return 0L;
+	/**
+	 * Bilanz des Portfolios.
+	 * 
+	 * @param datum
+	 *            Zeitpunkt in Jahren
+	 * @return bilanz in Cent
+	 */
+	public long getBilanz(int datum) {
+		return this.getZeitwert(datum) - this.getGesamtinvestition();
 	}
 
 }
