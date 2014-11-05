1cff0506fffe69756553b22145b6a716677d711d
sky2107 <felixmorillasnavas@gmail.com>
2014-11-05 12:34:15 +0100

    Update ImmobilieExistiertException.java
    Kommentare

--- Vermoegensverwaltung/src/com/ildbc/vermoegensabteilung/vermoegensverwaltung/vermoegensgegenstaende/immobilien/ImmobilieExistiertException.java
+++ Vermoegensverwaltung/src/com/ildbc/vermoegensabteilung/vermoegensverwaltung/vermoegensgegenstaende/immobilien/ImmobilieExistiertException.java
@@ -1,5 +1,13 @@
 package com.ildbc.vermoegensabteilung.vermoegensverwaltung.vermoegensgegenstaende.immobilien;
 
+/**
+ * wirft eine Exception wenn es eine Immobilie doppelt gibt
+ * 
+ * @author Adrian, Marcel und Felix
+ *
+ *date 5.11.2014
+ */
+
 public class ImmobilieExistiertException extends Exception {
 
 	private static final long serialVersionUID = 1L;
