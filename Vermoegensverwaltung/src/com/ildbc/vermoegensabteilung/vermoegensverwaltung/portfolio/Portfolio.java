f359ef2aefb83828f1e20290a200a5224a929f9f
sky2107 <felixmorillasnavas@gmail.com>
2014-11-05 11:46:42 +0100

    Update Portfolio.java
    Kommentare

--- Vermoegensverwaltung/src/com/ildbc/vermoegensabteilung/vermoegensverwaltung/portfolio/Portfolio.java
+++ Vermoegensverwaltung/src/com/ildbc/vermoegensabteilung/vermoegensverwaltung/portfolio/Portfolio.java
@@ -4,6 +4,17 @@ import java.util.List;
 
 import com.ildbc.vermoegensabteilung.vermoegensverwaltung.vermoegensgegenstaende.Vermoegensgegenstand;
 
+/**
+ * Portfolio ist ene Ansammlung der Vermoegenswerte
+ * Inhalt Aktien Festgelder Fonds Immobilien Rentenpapiere
+ * + den Zusatz der Optionen
+ * 
+ * sie werden mit hilfe einer Liste generiet
+ * 
+ * @author Adrian, Marcel und Felix
+ *date 5.11.2014
+ */
+
 public class Portfolio {
 
 	private final List<Vermoegensgegenstand> vermoegenswerte;
